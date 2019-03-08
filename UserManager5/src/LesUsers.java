e2d444c4af38464fdc0365c8d6fb673b76b4d963
LouisNdt <46976760+LouisNdt@users.noreply.github.com>
2019-03-08 17:05:36 +0100

    Louis Nardot

---
+++ UserManager5/src/LesUsers.java
@@ -0,0 +1,16 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class LesUsers {
+	
+	private List<User> _users;
+	public List<User> getUsers(){
+		return _users;
+	}
+	
+	public LesUsers() {
+		_users = new ArrayList<>();
+	}
+	
+}
